9171a95a6c29b43ee169d35c318c1135c273a192
Raja-saga <rajaganesh18904@gmail.com>
2025-09-13 09:55:00 +0530

    Server Push

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ io.on("connection",(socket)=>{
 
 
 //middlewares setup
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 
